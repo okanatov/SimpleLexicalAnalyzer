b0fc89e7e8a26e6980b7187ae448f9d329c3ec90
Oleg Kanatov <okanatov@gmail.com>
2018-08-09 22:50:19 +0300

    GetSize test a bit extended
    Signed-off-by: Oleg Kanatov <okanatov@gmail.com>

--- src/main/java/org/okanatov/lexer/DoubleBuffer.java
+++ src/main/java/org/okanatov/lexer/DoubleBuffer.java
@@ -15,253 +15,253 @@ import org.apache.logging.log4j.LogManager;
  * are returned to an application. The first half of the buffer is loaded, then.
  */
 final public class DoubleBuffer {
-    public static final int eof = 256;
+  public static final int eof = 256;
 
-    private static Logger logger = LogManager.getLogger(DoubleBuffer.class);
+  private static Logger logger = LogManager.getLogger(DoubleBuffer.class);
 
-    private int size;
-    private StringReader source;
-    private final int startOfFirst;
-    private final int endOfFirst;
-    private final int startOfSecond;
-    private final int endOfSecond;
-    private char[] buffer;
-    private int forward = 0;
-    private int begin = 0;
-    private Queue<Integer> loadedHalves = new LinkedList<>();
+  private int size;
+  private StringReader source;
+  private final int startOfFirst;
+  private final int endOfFirst;
+  private final int startOfSecond;
+  private final int endOfSecond;
+  private char[] buffer;
+  private int forward = 0;
+  private int begin = 0;
+  private Queue<Integer> loadedHalves = new LinkedList<>();
 
-    /**
-     * Takes size of a half of the buffer, i.e. the total buffer will be twice more, and {@link StringReader}
-     * object to read characters from.
-     *
-     *  @param s size of a half of the buffer. The total buffer size consisting of two halves is twice more
-     *  @param r {@link StringReader} object to read characters from
-     */
-    public DoubleBuffer(int s, StringReader r) throws IOException {
-        logger.traceEntry("Ctor: {}, {}", s, r);
+  /**
+   * Takes size of a half of the buffer, i.e. the total buffer will be twice more, and {@link StringReader}
+   * object to read characters from.
+   *
+   *  @param s size of a half of the buffer. The total buffer size consisting of two halves is twice more
+   *  @param r {@link StringReader} object to read characters from
+   */
+  public DoubleBuffer(int s, StringReader r) throws IOException {
+    logger.traceEntry("Ctor: {}, {}", s, r);
 
-        // Always check functions input
-        assert s != 0;
-        assert r != null;
+    // Always check functions input
+    assert s != 0;
+    assert r != null;
 
-        size = s;
-        source = r;
+    size = s;
+    source = r;
 
-        startOfFirst = 0;
-        endOfFirst = size;
-        startOfSecond = size + 1;
-        endOfSecond = size * 2 + 1;
+    startOfFirst = 0;
+    endOfFirst = size;
+    startOfSecond = size + 1;
+    endOfSecond = size * 2 + 1;
 
-        buffer = new char[size * 2 + 2];
+    buffer = new char[size * 2 + 2];
 
-        load(startOfFirst);
+    load(startOfFirst);
 
-        logger.exit();
-    }
+    logger.exit();
+  }
 
-    /** 
-     * Returns the next character from the internal buffer.
-     * In case DoubleBuffer.eof is reached at end of the first buffer half or
-     * at end of the second one, then reloads the opposite half. Returns
-     * DoubleBuffer.eof at once if this character is reached not at end of the halfs.
-     *
-     * @return the next character from the buffer or DoubleBuffer.eof if EOF is reached.
-     */
-    public char getc() throws IOException, Error {
-        logger.traceEntry("getc");
+  /** 
+   * Returns the next character from the internal buffer.
+   * In case DoubleBuffer.eof is reached at end of the first buffer half or
+   * at end of the second one, then reloads the opposite half. Returns
+   * DoubleBuffer.eof at once if this character is reached not at end of the halfs.
+   *
+   * @return the next character from the buffer or DoubleBuffer.eof if EOF is reached.
+   */
+  public char getc() throws IOException, Error {
+    logger.traceEntry("getc");
 
-        char ch = buffer[forward++];
+    char ch = buffer[forward++];
 
-        switch (ch) {
-            case eof:
-                if (forward == endOfFirst + 1) {
-                    logger.debug("EOF found at end of the first buffer. Loading the second...");
+    switch (ch) {
+      case eof:
+        if (forward == endOfFirst + 1) {
+          logger.debug("EOF found at end of the first buffer. Loading the second...");
 
-                    if (begin >= startOfSecond && begin < endOfSecond) {
-                      forward--;
-                      throw new Error("Buffer overflown");
-                    } else {
-                      loadedHalves.remove();
+          if (begin >= startOfSecond && begin < endOfSecond) {
+            forward--;
+            throw new Error("Buffer overflown");
+          } else {
+            loadedHalves.remove();
 
-                      if (loadedHalves.isEmpty()) {
-                        load(startOfSecond);
-                      }
+            if (loadedHalves.isEmpty()) {
+              load(startOfSecond);
+            }
 
-                      forward = startOfSecond;
+            forward = startOfSecond;
 
-                      if (begin == endOfFirst) {
-                        begin = startOfSecond;
-                      }
+            if (begin == endOfFirst) {
+              begin = startOfSecond;
+            }
 
-                      return getc();
-                    }
+            return getc();
+          }
 
-                } else if (forward == endOfSecond + 1) {
-                    logger.debug("EOF found at end of the second buffer. Loading the first...");
+        } else if (forward == endOfSecond + 1) {
+          logger.debug("EOF found at end of the second buffer. Loading the first...");
 
-                    if (begin >= startOfFirst && begin < endOfFirst) {
-                      forward--;
-                      throw new Error("Buffer overflown");
-                    } else {
-                      loadedHalves.remove();
+          if (begin >= startOfFirst && begin < endOfFirst) {
+            forward--;
+            throw new Error("Buffer overflown");
+          } else {
+            loadedHalves.remove();
 
-                      if (loadedHalves.isEmpty()) {
-                        load(startOfFirst);
-                      }
+            if (loadedHalves.isEmpty()) {
+              load(startOfFirst);
+            }
 
-                      forward = startOfFirst;
+            forward = startOfFirst;
 
-                      if (begin == endOfSecond) {
-                        begin = startOfFirst;
-                      }
-                      return getc();
-                    }
+            if (begin == endOfSecond) {
+              begin = startOfFirst;
+            }
+            return getc();
+          }
 
-                } else {
-                    logger.debug("EOF found. Quiting...");
-
-                    logger.exit();
-                    return eof;
-                }
-            default:
-                logger.debug("Non-EOF found: {}", ch);
-
-                logger.exit(ch);
-                return ch;
-        }
-    }
-
-    /**
-     * Returns size of the string.
-     *
-     * @return size of the string
-     */
-    public int getSize() {
-      logger.traceEntry("getSize");
-
-      if(ArePointersInSameHalf()) {
-        logger.debug("Begin and forward are in same half");
-        if (forward < begin) {
-          throw new Error("Buffer overflown");
-        }
-        return forward - begin;
-      } else {
-        if(begin >= startOfFirst && begin < endOfFirst) {
-          logger.debug("Begin and forward are not in same half. Begin in the first half");
-          return forward - begin - 1;
         } else {
-          logger.debug("Begin and forward are not in same half. Begin in the second half");
-          return endOfSecond - (begin - forward);
+          logger.debug("EOF found. Quiting...");
+
+          logger.exit();
+          return eof;
         }
+      default:
+        logger.debug("Non-EOF found: {}", ch);
+
+        logger.exit(ch);
+        return ch;
+    }
+  }
+
+  /**
+   * Returns size of the string.
+   *
+   * @return size of the string
+   */
+  public int getSize() {
+    logger.traceEntry("getSize");
+
+    if(ArePointersInSameHalf()) {
+      logger.debug("Begin and forward are in same half");
+      if (forward < begin) {
+        throw new Error("Buffer overflown");
+      }
+      return forward - begin;
+    } else {
+      if(begin >= startOfFirst && begin < endOfFirst) {
+        logger.debug("Begin and forward are not in same half. Begin in the first half");
+        return forward - begin - 1;
+      } else {
+        logger.debug("Begin and forward are not in same half. Begin in the second half");
+        return endOfSecond - (begin - forward);
+      }
+    }
+  }
+
+  /**
+   * Returns a string of characters located between the begin and the forward pointers. Makes begin point
+   * to the same position as forward does.
+   *
+   * @return a string of characters located between the begin and the forward pointers
+   */
+  public String getString() {
+    logger.traceEntry("getString");
+
+    String result = "";
+    String buffer = new String(this.buffer); // Convert buffer array to String
+
+    if(ArePointersInSameHalf()) {
+      logger.debug("Begin and forward are in same half");
+
+      if (forward < begin) {
+        throw new Error("Buffer overflown");
+      }
+
+      result = buffer.substring(begin, forward);
+    } else {
+      if(begin >= startOfFirst && begin < endOfFirst) {
+        logger.debug("Begin and forward are not in same half. Begin in the first half");
+
+        result = buffer.substring(begin, endOfFirst) + buffer.substring(startOfSecond, forward);
+      } else {
+        logger.debug("Begin and forward are not in same half. Begin in the second half");
+
+        result = buffer.substring(begin, endOfSecond) + buffer.substring(startOfFirst, forward);
       }
     }
 
-    /**
-     * Returns a string of characters located between the begin and the forward pointers. Makes begin point
-     * to the same position as forward does.
-     *
-     * @return a string of characters located between the begin and the forward pointers
-     */
-    public String getString() {
-        logger.traceEntry("getString");
+    begin = forward;
 
-        String result = "";
-        String buffer = new String(this.buffer); // Convert buffer array to String
+    logger.exit(result);
+    return result;
+  }
 
-        if(ArePointersInSameHalf()) {
-          logger.debug("Begin and forward are in same half");
+  /**
+   * Returns one last-read character back to the buffer so that next getc() will return it again.
+   */
+  public void ungetc() {
+    logger.traceEntry("ungetc");
 
-          if (forward < begin) {
-            throw new Error("Buffer overflown");
-          }
+    forward = (endOfSecond + 1 + (forward - 1)) % (endOfSecond + 1);
 
-          result = buffer.substring(begin, forward);
-        } else {
-            if(begin >= startOfFirst && begin < endOfFirst) {
-                logger.debug("Begin and forward are not in same half. Begin in the first half");
-
-                result = buffer.substring(begin, endOfFirst) + buffer.substring(startOfSecond, forward);
-            } else {
-                logger.debug("Begin and forward are not in same half. Begin in the second half");
-
-                result = buffer.substring(begin, endOfSecond) + buffer.substring(startOfFirst, forward);
-            }
-        }
-
-        begin = forward;
-
-        logger.exit(result);
-        return result;
+    if (buffer[forward] == DoubleBuffer.eof) {
+      if (forward == endOfFirst) {
+        loadedHalves.add(2);
+        --forward;
+      } else if (forward == endOfSecond) {
+        loadedHalves.add(1);
+        --forward;
+      } else {
+        logger.debug("End of stream has been reached");
+      }
     }
 
-    /**
-     * Returns one last-read character back to the buffer so that next getc() will return it again.
-     */
-    public void ungetc() {
-        logger.traceEntry("ungetc");
+    logger.exit();
+  }
 
-        forward = (endOfSecond + 1 + (forward - 1)) % (endOfSecond + 1);
-
-        if (buffer[forward] == DoubleBuffer.eof) {
-            if (forward == endOfFirst) {
-                loadedHalves.add(2);
-                --forward;
-            } else if (forward == endOfSecond) {
-                loadedHalves.add(1);
-                --forward;
-            } else {
-                logger.debug("End of stream has been reached");
-            }
+  /**
+   * Checks if the begin and forward pointers are in the same half.
+   *
+   * @return true if both pointers are in the same half, false otherwise
+   */
+  private boolean ArePointersInSameHalf() {
+    if ((begin >= startOfFirst && begin <= endOfFirst) &&       // We can create a method to check
+        (forward >= startOfFirst && forward <= endOfFirst)) {   // whether a pointer is in first half or not
+      return true;
         }
 
-        logger.exit();
+    if ((begin >= startOfSecond && begin <= endOfSecond) &&
+        (forward >= startOfSecond && forward <= endOfSecond)) {
+      return true;
+        }
+
+    return false;
+  }
+
+  /** 
+   * Loads characters from {@link StringReader} to the internal buffer starting
+   * from position pos.
+   *
+   * @param pos Offset at which to start writing characters in the buffer
+   */
+  private void load(int pos) throws IOException {
+    logger.traceEntry("load: {}", pos);
+
+    assert((pos == startOfFirst) || (pos == startOfSecond));
+
+    int len = source.read(buffer, pos, size);
+
+    if (len <= 0) {
+      logger.debug("End of the stream has been reached");
+      buffer[pos] = eof;
+    } else {
+      buffer[pos + len] = eof;
     }
 
-    /**
-     * Checks if the begin and forward pointers are in the same half.
-     *
-     * @return true if both pointers are in the same half, false otherwise
-     */
-    private boolean ArePointersInSameHalf() {
-        if ((begin >= startOfFirst && begin <= endOfFirst) &&       // We can create a method to check
-            (forward >= startOfFirst && forward <= endOfFirst)) {   // whether a pointer is in first half or not
-                return true;
-        }
-
-        if ((begin >= startOfSecond && begin <= endOfSecond) &&
-            (forward >= startOfSecond && forward <= endOfSecond)) {
-                return true;
-        }
-
-        return false;
+    if (pos == startOfFirst) {
+      loadedHalves.add(1);
+    } else {
+      loadedHalves.add(2);
     }
 
-    /** 
-     * Loads characters from {@link StringReader} to the internal buffer starting
-     * from position pos.
-     *
-     * @param pos Offset at which to start writing characters in the buffer
-     */
-    private void load(int pos) throws IOException {
-        logger.traceEntry("load: {}", pos);
-
-        assert((pos == startOfFirst) || (pos == startOfSecond));
-
-        int len = source.read(buffer, pos, size);
-
-        if (len <= 0) {
-            logger.debug("End of the stream has been reached");
-            buffer[pos] = eof;
-        } else {
-            buffer[pos + len] = eof;
-        }
-
-        if (pos == startOfFirst) {
-            loadedHalves.add(1);
-        } else {
-            loadedHalves.add(2);
-        }
-
-        logger.exit();
-    }
+    logger.exit();
+  }
 }
--- src/test/java/org/okanatov/lexer/DoubleBufferTest.java
+++ src/test/java/org/okanatov/lexer/DoubleBufferTest.java
@@ -11,220 +11,226 @@ import org.junit.Ignore;
 /**
  * This class "unit"tests the {@DoubleBuffer} class.
  */
-public class DoubleBufferTest
-{
-    private DoubleBuffer b;
+public class DoubleBufferTest {
+  private DoubleBuffer b;
 
-    @Test(expected = AssertionError.class)
-    public void testCtorChecksNullReader() {
-        try {
-            b = new DoubleBuffer(5, null);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  @Test(expected = AssertionError.class)
+  public void testCtorChecksNullReader() {
+    try {
+      b = new DoubleBuffer(5, null);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test(expected = AssertionError.class)
+  public void testCtorChecksZeroSize() {
+    try {
+      b = new DoubleBuffer(0, null);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void testGetcReturnsEOFOnEmptyReader() {
+    try {
+      b = new DoubleBuffer(5, new StringReader(""));
+      assertEquals(DoubleBuffer.eof, b.getc());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void testGetcCanReturnEOFOnAllCharsRead() {
+    try {
+      b = new DoubleBuffer(5, new StringReader("0"));
+      assertEquals('0', b.getc());
+      assertEquals(DoubleBuffer.eof, b.getc());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void testGetcCanLoadBuffersAndReturnEOFOnAllCharsRead() {
+    String input = "0123456789abcdef";
+
+    try {
+      b = new DoubleBuffer(5, new StringReader(input));
+
+      char arr[] = input.toCharArray();
+      for (char c : arr) {
+        assertEquals(c, b.getc());
+        System.out.println(Character.toString(c));
+        assertEquals(Character.toString(c), b.getString());
+      }
+
+      assertEquals(DoubleBuffer.eof, b.getc());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void testGetStringCanReturnDiffBetweenPointers() {
+    try {
+      b = new DoubleBuffer(5, new StringReader("012"));
+      assertEquals('0', b.getc());
+      assertEquals('1', b.getc());
+      assertEquals('2', b.getc());
+      assertEquals("012", b.getString());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void testGetStringReturnsEmptyStringIfPointersAreSame() {
+    try {
+      b = new DoubleBuffer(5, new StringReader("012"));
+      assertEquals("", b.getString());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void testGetStringReturnsEmptyStringIfPointersAreSameAfterSomeRead() {
+    try {
+      b = new DoubleBuffer(5, new StringReader("0123456"));
+      assertEquals('0', b.getc());
+      assertEquals('1', b.getc());
+      assertEquals('2', b.getc());
+      assertEquals("012", b.getString());
+      assertEquals("", b.getString());
+      assertEquals('3', b.getc());
+      assertEquals("3", b.getString());
+      assertEquals('4', b.getc());
+      assertEquals('5', b.getc());
+      assertEquals('6', b.getc());
+      assertEquals("456", b.getString());
+      assertEquals(DoubleBuffer.eof, b.getc());
+      b.ungetc();
+      assertEquals("", b.getString());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void testNewCharactersAreNotLoadedIfBufferIsOverflown() {
+    try {
+      b = new DoubleBuffer(5, new StringReader("0123456789abcdef"));
+
+      for (int i = 0; i < 10; i++) {
+        assertEquals((char) (i + '0'), b.getc());
+      }
+
+      b.getc(); // causes Exception
+    } catch (Error e) {
+      System.out.println(e);
+    } catch (IOException e1) {
+      e1.printStackTrace();
     }
 
-    @Test(expected = AssertionError.class)
-    public void testCtorChecksZeroSize() {
-        try {
-            b = new DoubleBuffer(0, null);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    try {
+      assertEquals("0123456789", b.getString());
+      assertEquals('a', b.getc());
+      assertEquals('b', b.getc());
+      assertEquals('c', b.getc());
+      assertEquals('d', b.getc());
+      assertEquals('e', b.getc());
+      assertEquals('f', b.getc());
+      assertEquals(DoubleBuffer.eof, b.getc());
+      b.ungetc();
+      assertEquals("abcdef", b.getString());
+    } catch (IOException e2) {
+      e2.printStackTrace();
+    }
+  }
+
+  @Test
+  public void testNewCharactersAreNotLoadedIfTheyWereBefore() {
+    try {
+      b = new DoubleBuffer(5, new StringReader("0123456"));
+
+      assertEquals('0', b.getc());
+      assertEquals('1', b.getc());
+      assertEquals('2', b.getc());
+      assertEquals('3', b.getc());
+      assertEquals('4', b.getc());
+      assertEquals('5', b.getc());
+      assertEquals('6', b.getc());
+      b.ungetc();
+      assertEquals('6', b.getc());
+      b.ungetc();
+      b.ungetc();
+      b.ungetc();
+      assertEquals('4', b.getc());
+      assertEquals('5', b.getc());
+      assertEquals('6', b.getc());
+      assertEquals(DoubleBuffer.eof, b.getc());
+      b.ungetc();
+      b.ungetc();
+      assertEquals('6', b.getc());
+      assertEquals(DoubleBuffer.eof, b.getc());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void testUngetcCharacter() {
+    try {
+      b = new DoubleBuffer(3, new StringReader("0123456789abcdef"));
+
+      assertEquals('0', b.getc());
+      assertEquals('1', b.getc());
+      assertEquals('2', b.getc());
+      assertEquals('3', b.getc());
+      b.ungetc();
+      b.ungetc();
+      assertEquals('2', b.getc());
+      assertEquals('3', b.getc());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void testGetSize() {
+    try {
+      b = new DoubleBuffer(5, new StringReader("0123456789abcdef"));
+
+      b.getc();
+      b.getc();
+      b.getc();
+      assertEquals(3, b.getSize());
+      b.getc();
+      b.getc();
+      b.getc();
+      assertEquals(6, b.getSize());
+      b.getString();
+      b.getc();
+      b.getc();
+      assertEquals(2, b.getSize());
+      b.getc();
+      b.getc();
+      b.getc();
+      b.getc();
+      b.getc();
+      assertEquals(7, b.getSize());
+      b.getc();
+      b.getc();
+      b.getc();
+    } catch (Error e) {
+      System.out.println(e);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
 
-    @Test
-    public void testGetcReturnsEOFOnEmptyReader() {
-        try {
-            b = new DoubleBuffer(5, new StringReader(""));
-            assertEquals(DoubleBuffer.eof, b.getc());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void testGetcCanReturnEOFOnAllCharsRead() {
-        try {
-            b = new DoubleBuffer(5, new StringReader("0"));
-            assertEquals('0', b.getc());
-            assertEquals(DoubleBuffer.eof, b.getc());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void testGetcCanLoadBuffersAndReturnEOFOnAllCharsRead() {
-        String input = "0123456789abcdef";
-
-        try {
-            b = new DoubleBuffer(5, new StringReader(input));
-
-            char arr[] = input.toCharArray();
-            for (char c : arr) {
-                assertEquals(c, b.getc());
-                System.out.println(Character.toString(c));
-                assertEquals(Character.toString(c), b.getString());
-            }
-
-            assertEquals(DoubleBuffer.eof, b.getc());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void testGetStringCanReturnDiffBetweenPointers() {
-        try {
-            b = new DoubleBuffer(5, new StringReader("012"));
-            assertEquals('0', b.getc());
-            assertEquals('1', b.getc());
-            assertEquals('2', b.getc());
-            assertEquals("012", b.getString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void testGetStringReturnsEmptyStringIfPointersAreSame() {
-        try {
-            b = new DoubleBuffer(5, new StringReader("012"));
-            assertEquals("", b.getString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void testGetStringReturnsEmptyStringIfPointersAreSameAfterSomeRead() {
-        try {
-            b = new DoubleBuffer(5, new StringReader("0123456"));
-            assertEquals('0', b.getc());
-            assertEquals('1', b.getc());
-            assertEquals('2', b.getc());
-            assertEquals("012", b.getString());
-            assertEquals("", b.getString());
-            assertEquals('3', b.getc());
-            assertEquals("3", b.getString());
-            assertEquals('4', b.getc());
-            assertEquals('5', b.getc());
-            assertEquals('6', b.getc());
-            assertEquals("456", b.getString());
-            assertEquals(DoubleBuffer.eof, b.getc());
-            b.ungetc();
-            assertEquals("", b.getString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void testNewCharactersAreNotLoadedIfBufferIsOverflown() {
-        try {
-            b = new DoubleBuffer(5, new StringReader("0123456789abcdef"));
-
-            for (int i = 0; i < 10; i++) {
-                assertEquals((char) (i + '0'), b.getc());
-            }
-
-            b.getc(); // causes Exception
-        } catch (Error e) {
-          System.out.println(e);
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-
-        try {
-            assertEquals("0123456789", b.getString());
-            assertEquals('a', b.getc());
-            assertEquals('b', b.getc());
-            assertEquals('c', b.getc());
-            assertEquals('d', b.getc());
-            assertEquals('e', b.getc());
-            assertEquals('f', b.getc());
-            assertEquals(DoubleBuffer.eof, b.getc());
-            b.ungetc();
-            assertEquals("abcdef", b.getString());
-        } catch (IOException e2) {
-            e2.printStackTrace();
-        }
-    }
-
-    @Test
-    public void testNewCharactersAreNotLoadedIfTheyWereBefore() {
-        try {
-            b = new DoubleBuffer(5, new StringReader("0123456"));
-
-            assertEquals('0', b.getc());
-            assertEquals('1', b.getc());
-            assertEquals('2', b.getc());
-            assertEquals('3', b.getc());
-            assertEquals('4', b.getc());
-            assertEquals('5', b.getc());
-            assertEquals('6', b.getc());
-            b.ungetc();
-            assertEquals('6', b.getc());
-            b.ungetc();
-            b.ungetc();
-            b.ungetc();
-            assertEquals('4', b.getc());
-            assertEquals('5', b.getc());
-            assertEquals('6', b.getc());
-            assertEquals(DoubleBuffer.eof, b.getc());
-            b.ungetc();
-            b.ungetc();
-            assertEquals('6', b.getc());
-            assertEquals(DoubleBuffer.eof, b.getc());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void testUngetcCharacter() {
-        try {
-            b = new DoubleBuffer(3, new StringReader("0123456789abcdef"));
-
-            assertEquals('0', b.getc());
-            assertEquals('1', b.getc());
-            assertEquals('2', b.getc());
-            assertEquals('3', b.getc());
-            b.ungetc();
-            b.ungetc();
-            assertEquals('2', b.getc());
-            assertEquals('3', b.getc());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void testGetSize() {
-        try {
-          b = new DoubleBuffer(5, new StringReader("0123456789abcdef"));
-
-          b.getc();
-          b.getc();
-          b.getc();
-          assertEquals(3, b.getSize());
-          b.getc();
-          b.getc();
-          b.getc();
-          assertEquals(6, b.getSize());
-          b.getString();
-          b.getc();
-          b.getc();
-          assertEquals(2, b.getSize());
-          b.getc();
-          b.getc();
-          b.getc();
-          b.getc();
-          b.getc();
-          assertEquals(7, b.getSize());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    assertEquals(9, b.getSize());
+  }
 }
