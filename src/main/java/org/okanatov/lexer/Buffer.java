1ad1a32d382a2d852b75647c76ac78494553d640
Oleg Kanatov <okanatov@gmail.com>
2018-08-20 19:05:28 +0300

    Simple Buffer class implementation for ChainedLexer
    Signed-off-by: Oleg Kanatov <okanatov@gmail.com>

--- src/main/java/org/okanatov/lexer/Buffer.java
+++ src/main/java/org/okanatov/lexer/Buffer.java
@@ -8,136 +8,30 @@ class Buffer
 {
     private final int bufferSize;
     private final StringReader reader;
-    private int begin = 0;
-    private int forward = 0;
     private final char[] current;
-
-    private final int lastElemOfFirstBuffer;
-    private final int lastElemOfSecondBuffer;
+    private int pos;
+    private int len;
 
     public Buffer(int bufferSize, StringReader reader) {
         this.bufferSize = bufferSize;
-        this.lastElemOfFirstBuffer = bufferSize;
-        this.lastElemOfSecondBuffer = bufferSize * 2 + 1;
-        int totalBufferSize = bufferSize * 2 + 2;
-
         this.reader = reader;
-        this.current = new char[totalBufferSize];
-        this.current[lastElemOfFirstBuffer] = current[lastElemOfSecondBuffer] = '$';
-
-        tryRead();
+        this.current = new char[bufferSize];
+        this.pos = 0;
+        this.len = 0;
     }
 
-    public char read() {
-        char ch = current[forward++];
-        ensureNotOverflown();
-
-        if (ch != '$')
-            return ch;
-
-        if (forward == lastElemOfFirstBuffer + 1) {
-            if (!tryRead()) return returnEnd();
-
-            return current[forward++];
-        } else if (forward == lastElemOfSecondBuffer + 1) {
-            forward = 0;
-            if (!tryRead()) return returnEnd();
-
-            return current[forward++];
-        } else {
-            return returnEnd();
-        }
-    }
-
-    public String getString() {
-        if (isBeginAndForwardInOneBuffer()) {
-            return new String(Arrays.copyOfRange(current, begin, forward));
-        } else {
-            if (isPosInFirstBuffer(begin)) {
-                return new String(Arrays.copyOfRange(current, begin, lastElemOfFirstBuffer)) +
-                        new String(Arrays.copyOfRange(current, lastElemOfFirstBuffer + 1, forward));
-            } else {
-                return new String(Arrays.copyOfRange(current, begin, lastElemOfSecondBuffer)) +
-                        new String(Arrays.copyOfRange(current, 0, forward));
-
+    public char getc() throws IOException {
+        if (pos == len) {
+            len = reader.read(current, 0, bufferSize);
+            if (len == -1) {
+                pos = -1;
+                return '0';
             }
-
-        }
-    }
-
-    public void setBegin(int newBegin) {
-        assert ((newBegin >= 0 && newBegin < lastElemOfSecondBuffer));
-        if (newBegin >= lastElemOfFirstBuffer) newBegin++;
-        if (newBegin == lastElemOfSecondBuffer) newBegin = 0;
-
-        this.begin = newBegin;
-    }
-
-    public void setForward(int newForward) {
-        assert ((newForward >= 0 && newForward < lastElemOfSecondBuffer));
-        if (newForward >= lastElemOfFirstBuffer) newForward++;
-        if (newForward == lastElemOfSecondBuffer) newForward = 0;
-
-        this.forward = newForward;
-    }
-
-    public int getBegin() {
-        int temp = begin;
-        if (temp > lastElemOfFirstBuffer) temp--;
-
-        return temp;
-    }
-
-    public int getForward() {
-        int temp = forward;
-        if (temp > lastElemOfFirstBuffer) temp--;
-
-        return temp;
-    }
-
-    private boolean isBeginAndForwardInOneBuffer() {
-        return (isPosInFirstBuffer(begin) && isPosInFirstBuffer(forward)) ||
-                (isPosInSecondBuffer(begin) && isPosInSecondBuffer(forward));
-    }
-
-    private boolean isPosInFirstBuffer(final int pos) {
-        return pos >= 0 && pos <= lastElemOfFirstBuffer;
-    }
-
-    private boolean isPosInSecondBuffer(final int pos) {
-        return pos > lastElemOfFirstBuffer && pos <= lastElemOfSecondBuffer;
-    }
-
-    private char returnEnd() {
-        --forward;
-        assert (current[forward] == '$');
-        return current[forward];
-    }
-
-    private boolean tryRead() {
-        int len = read(reader, forward);
-        if (len <= 0) {
-            current[forward] = '$';
-            return false;
-        }
-        if (len < bufferSize) current[forward + len] = '$';
-
-        return true;
-    }
-
-    private int read(StringReader reader, final int pos) {
-        int len = 0;
-        try {
-            len = reader.read(current, pos, bufferSize);
-        } catch (IOException e) {
-            e.printStackTrace();
+            pos = 0;
         }
 
-        return len;
-    }
+        char ch = current[pos++];
 
-    private void ensureNotOverflown() {
-        if (isBeginAndForwardInOneBuffer() && (begin > forward))
-            throw new Error("buffer overflow");
+        return ch;
     }
 }
--- src/test/java/org/okanatov/lexer/BufferTest.java
+++ src/test/java/org/okanatov/lexer/BufferTest.java
@@ -1,9 +1,8 @@
 package org.okanatov.lexer;
 
-import org.junit.Before;
 import org.junit.Test;
-
 import java.io.StringReader;
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -11,84 +10,27 @@ public class BufferTest
 {
     private Buffer buffer;
 
-    @Before
-    public void setUp() throws Exception {
-        buffer = new Buffer(6, new StringReader("01234567890123456789"));
-    }
-
     @Test
-    public void testBufferReturnsOneSymbolAtTimeIfBeginAndForwardAreTheSame()
+    public void testBufferReturnsOneCharAtTime() throws IOException
     {
-        int i = 0;
-        String text;
-        for (char ch = buffer.read(); ch != '$'; ch = buffer.read()) {
-            text = buffer.getString();
-            buffer.setBegin(buffer.getForward());
-
-            assertEquals(Integer.valueOf(i++).toString(), text);
-            i %= 10;
-        }
-    }
-
-    @Test
-    public void testBeginAndForwardInFirstBuffer() {
-        buffer.setForward(4);
-        buffer.setBegin(1);
-        assertEquals("123", buffer.getString());
-    }
-
-    @Test
-    public void testBeginAndForwardInDifferentBuffersAndBeginInFirst() {
-        for (int i = 0; i < 12; i++) buffer.read();
-        buffer.setBegin(4);
-
-        assertEquals("45678901", buffer.getString());
-    }
-
-    @Test
-    public void testBeginAndForwardInDifferentBuffersAndBeginInSecond() {
-        for (int i = 0; i < 8; i++) buffer.read();
-        buffer.setBegin(8);
-        for (int i = 0; i < 8; i++) buffer.read();
-
-        assertEquals("89012345", buffer.getString());
-    }
-
-    @Test
-    public void testBeginForwardInSecond() {
-        for (int i = 0; i < 12; i++) buffer.read();
-        buffer.setBegin(8);
-
-        assertEquals("8901", buffer.getString());
-    }
-
-    @Test
-    public void testGetAndSetForward() {
-        buffer.setForward(5);
-        assertEquals(5, buffer.getForward());
-
-        buffer.setForward(11);
-        assertEquals(11, buffer.getForward());
-
-        buffer.setForward(12);
-        assertEquals(0, buffer.getForward());
-    }
-
-    @Test
-    public void testGetAndSetBegin() {
-        buffer.setBegin(5);
-        assertEquals(5, buffer.getBegin());
-
-        buffer.setBegin(11);
-        assertEquals(11, buffer.getBegin());
-
-        buffer.setBegin(12);
-        assertEquals(0, buffer.getBegin());
-    }
-
-    @Test(expected = Error.class)
-    public void testBufferOverflow() {
-        buffer.setBegin(4);
-        buffer.read();
+        Buffer b = new Buffer(5, new StringReader("0123456789abcdef"));
+        assertEquals('0', b.getc());
+        assertEquals('1', b.getc());
+        assertEquals('2', b.getc());
+        assertEquals('3', b.getc());
+        assertEquals('4', b.getc());
+        assertEquals('5', b.getc());
+        assertEquals('6', b.getc());
+        assertEquals('7', b.getc());
+        assertEquals('8', b.getc());
+        assertEquals('9', b.getc());
+        assertEquals('a', b.getc());
+        assertEquals('b', b.getc());
+        assertEquals('c', b.getc());
+        assertEquals('d', b.getc());
+        assertEquals('e', b.getc());
+        assertEquals('f', b.getc());
+        assertEquals('0', b.getc());
+        assertEquals('0', b.getc());
     }
 }
